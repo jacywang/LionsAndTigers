729f7cdcecb482a7a7e5dd115e494eee00973d62
Jacy Wang <jacywj@gmail.com>
2015-02-06 20:35:00 -0500

    Initialize repo

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by JIAN WANG on 2/6/15.
+//  Copyright (c) 2015 JACY WANG. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger()
+        myTiger.age = 3
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal Tiger"
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        println("My tiger's name is \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed) and its image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedlabel.text = myTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.age = 2
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.age = 5
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.age = 1
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +57,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+    }
 
 }
 
